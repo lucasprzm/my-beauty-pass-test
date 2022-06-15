ec920febb426ce326fea4488e70dfd477ef57024
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 20:43:37 -0300

    feat: connecting data source

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import "reflect-metadata";
+import "./database/data-source";
 const app = express();
 
 app.listen(3333, () => {
-  console.log("Running on port 3333!");
+  console.log("Server running locally on port 3333!");
 });
