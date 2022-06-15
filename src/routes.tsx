ad61693bad69004cd52e458196a8bfaac1f3cdd9
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 16:26:43 -0300

    feat: created routes

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes as RoutesWrapper } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+
+export function Routes() {
+  return (
+    <RoutesWrapper>
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+    </RoutesWrapper>
+  );
+}
