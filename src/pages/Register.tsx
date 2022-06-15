6836e464eebc7d7f5783f8c12d3cb2c02f898bfd
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 17:14:00 -0300

    feat: new user page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,1 +1,23 @@
-export function Register() {}
+import Logo from "../assets/logo.jpg";
+
+export function Register() {
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <img src={Logo} />
+      </div>
+      <span className="font-bold text-xl mb-4">Cadastro</span>
+      <form className="flex flex-col w-1/4">
+        <label htmlFor="">Nome</label>
+        <input className="text-black rounded-xl mt-2 mb-4" type="text" />
+        <label htmlFor="">E-mail</label>
+        <input className="text-black rounded-xl mt-2 mb-4" type="email" />
+        <label htmlFor="">Senha</label>
+        <input className="text-black rounded-xl mt-2 mb-4" type="password" />
+        <button className="border-2 rounded-xl mt-2 p-1" type="submit">
+          Cadastrar
+        </button>
+      </form>
+    </div>
+  );
+}
