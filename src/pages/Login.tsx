c1ee744c45e08d9853c2280f4ce58046be508027
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 17:13:39 -0300

    feat: new login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,1 +1,29 @@
-export function Login() {}
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.jpg";
+import { Register } from "./Register";
+
+export function Login() {
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <img src={Logo} />
+      </div>
+      <span className="font-bold text-xl mb-4">Login</span>
+      <form className="flex flex-col w-1/4">
+        <label htmlFor="">E-mail</label>
+        <input className="text-black rounded-xl mt-2 mb-4" type="email" />
+        <label htmlFor="">Senha</label>
+        <input className="text-black rounded-xl mt-2 mb-4" type="password" />
+        <button className="border-2 rounded-xl mt-2 mb-4 p-1" type="submit">
+          Entrar
+        </button>
+      </form>
+      <span>
+        Não tem cadastro?{" "}
+        <Link className="hover:text-[#FECB9B]" to="/register">
+          Cadastre-se aqui!
+        </Link>{" "}
+      </span>
+    </div>
+  );
+}
