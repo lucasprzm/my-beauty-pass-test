a27b6010fd0191e18ec174d30ab91c04094e90a3
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 20:44:56 -0300

    feat: create users table migration

---
+++ server/src/migrations/1655335921442-CreateUsers.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1655335921442 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "userId",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "userName",
+            type: "varchar",
+          },
+          {
+            name: "userEmail",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable("users");
+  }
+}
