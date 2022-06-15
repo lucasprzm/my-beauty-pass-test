313cb39e13d26a0c437644a943b2f7355d360f8c
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 17:20:10 -0300

    feat: home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,12 @@
-export function Home() {}
+import Logo from "../assets/logo.jpg";
+
+export function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <img src={Logo} />
+      </div>
+      <span className="font-bold text-xl">Seja bem-vindo(a) *nome da pessoa</span>
+    </div>
+  );
+}
