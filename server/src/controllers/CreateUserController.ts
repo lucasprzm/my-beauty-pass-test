1664a20c0a03b9feaa63b73e84e7163399f8f69a
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 21:43:26 -0300

    feat: create user endpoint

---
+++ server/src/controllers/CreateUserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { User } from "../entities/User";
+import { AppDataSource } from "../database/data-source";
+import { hash } from "bcryptjs";
+
+export class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const userRepository = AppDataSource.getRepository(User);
+    const { name, email, password } = request.body;
+    const passwordHash = await hash(password, 10);
+    if (!email) {
+      throw new Error("Incorrect email!");
+    }
+    const userAlreadyExists = await userRepository.findOneBy({ userEmail: email });
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+    const user = userRepository.create({
+      userName: name,
+      userEmail: email,
+      userPassword: passwordHash,
+    });
+    await userRepository.save(user);
+    return response.status(200).json(user);
+  }
+}
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { CreateUserController } from "../controllers/CreateUserController";
 const usersRouter = Router();
-
+const createUserController = new CreateUserController();
 // Registro de novo usuário
-usersRouter.post("/new");
+usersRouter.post("/new", createUserController.handle);
 
 // Login de usuário
 usersRouter.post("/login");
