2e367ddef1a406f266d44402b338b3be5960b241
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 23:14:34 -0300

    feat: user login

---
+++ server/src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../database/data-source";
+import { User } from "../entities/User";
+import { compareSync } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import "dotenv/config";
+
+export class AuthenticateUserController {
+  async handle(request: Request, response: Response) {
+    const userRepository = AppDataSource.getRepository(User);
+    const { email, password } = request.body;
+    const user = await userRepository.findOneBy({ userEmail: email });
+    if (!user) {
+      throw new Error("Email/Password Incorrect!");
+    }
+    const passwordMatch = compareSync(password, user.userPassword);
+    if (!passwordMatch) {
+      throw new Error("Email/Password Incorrect!");
+    }
+    const token = sign({ email: user.userEmail }, process.env.SECRET_KEY, {
+      subject: user.userId,
+      expiresIn: "1d",
+    });
+    return response.status(200).json(token);
+  }
+}
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "../controllers/AuthenticateUserController";
 import { CreateUserController } from "../controllers/CreateUserController";
 const usersRouter = Router();
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 // Registro de novo usuário
 usersRouter.post("/new", createUserController.handle);
 
 // Login de usuário
-usersRouter.post("/login");
+usersRouter.post("/login", authenticateUserController.handle);
 
 export { usersRouter };
