000128a1adf29926bf58addc849ee20673c55524
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-20 16:35:36 -0300

    feat: user login

--- front-end-react/src/pages/Login.tsx
+++ front-end-react/src/pages/Login.tsx
@@ -1,24 +1,62 @@
-import { Link } from "react-router-dom";
+import { useState, FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.jpg";
+import { api } from "../services/api";
+import { toast } from "react-toastify";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    api
+      .post("/users/login", {
+        email,
+        password,
+      })
+      .then(({ status, data }) => {
+        if (status === 200) {
+          toast.success(data.message);
+          localStorage.setItem("token", data.token);
+          navigate("/home");
+        }
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error);
+      });
+  }
+
   return (
     <div className="flex flex-col items-center">
       <div>
         <img src={Logo} />
       </div>
       <span className="font-bold text-xl mb-4">Login</span>
-      <form className="flex flex-col w-1/4" onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="">E-mail</label>
-        <input className="text-black rounded-xl mt-2 mb-4" type="email" autoComplete="username" />
-        <label htmlFor="">Senha</label>
+      <form className="flex flex-col w-1/4" onSubmit={handleSubmit}>
+        <label htmlFor="email">E-mail</label>
+        <input
+          className="text-black rounded-xl mt-2 mb-4"
+          id="email"
+          type="email"
+          autoComplete="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Senha</label>
         <input
           className="text-black rounded-xl mt-2 mb-4"
           type="password"
           autoComplete="current-password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
         <button className="border-2 rounded-xl mt-2 mb-4 p-1" type="submit">
-          <Link to="/home">Entrar</Link>
+          Entrar
         </button>
       </form>
       <span>
