bdbeb7505e620a09ed8b9f8071b6d426a52f2aec
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-20 12:02:09 -0300

    feat: toast added for alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import { Routes } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer autoClose={3000} />
       <Routes />
     </BrowserRouter>
   );
