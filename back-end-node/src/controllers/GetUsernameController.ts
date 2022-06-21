8b5c61322aa47d9fda7b321fd8aa633cbb75db94
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-20 21:34:28 -0300

    refactor: unused error

--- back-end-node/src/controllers/GetUsernameController.ts
+++ back-end-node/src/controllers/GetUsernameController.ts
@@ -12,9 +12,6 @@ export class GetUsernameController {
   async handle(request: Request, response: Response) {
     const userRepository = AppDataSource.getRepository(User);
     const authToken = request.headers.authorization;
-    if (!authToken) {
-      throw new Error("Usuário não está logado!");
-    }
     const [, token] = authToken.split(" ");
     const { sub } = verify(token, process.env.SECRET_KEY) as IPayLoad;
     const { userName } = await userRepository.findOneBy({ userId: sub });
